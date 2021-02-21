d66917a17847ab1353c05a8275dfef46a8360657
Piyush Daga <pdaga7775@conestogac.on.ca>
2021-02-20 22:42:10 -0500

    connected to git

---
+++ assign1_rect/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign1_rect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
